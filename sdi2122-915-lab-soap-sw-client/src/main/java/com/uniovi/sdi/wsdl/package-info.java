364717d7413bd6b96f60126e09c893c4567d233c
Miguel <migueelsuarez@gmail.com>
2022-05-01 20:29:03 +0200

    SDI-2122-915-SW-11.1- Servicio SOAP Cliente.

---
+++ sdi2122-915-lab-soap-sw-client/src/main/java/com/uniovi/sdi/wsdl/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por Eclipse Implementation of JAXB v2.3.6 
+// Visite https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2022.05.01 a las 08:26:44 PM CEST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://uniovi.com/sdi/soap/ws", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.uniovi.sdi.wsdl;
